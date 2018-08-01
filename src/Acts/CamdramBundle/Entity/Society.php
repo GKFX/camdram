a08f12d97591a64ff1fb7950a9f5dd0eaacb84a5
George Bateman <george.bateman16@gmail.com>
2018-08-01 23:35:20 +0100

    Remove shows array from Society

--- src/Acts/CamdramBundle/Entity/Society.php
+++ src/Acts/CamdramBundle/Entity/Society.php
@@ -15,59 +15,11 @@ use Acts\CamdramApiBundle\Configuration\Annotation as Api;
  */
 class Society extends Organisation
 {
-    /**
-     * @ORM\OneToMany(targetEntity="Show", mappedBy="society")
-     * @Api\Link(route="get_society_shows", params={"identifier": "object.getSlug()"})
-     */
-    private $shows;
-
     private $entity_type = 'society';
 
     public function getEntityType()
     {
         return $this->entity_type;
-    }
-
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->shows = new \Doctrine\Common\Collections\ArrayCollection();
-    }
-
-    /**
-     * Add shows
-     *
-     * @param \Acts\CamdramBundle\Entity\Show $shows
-     *
-     * @return Society
-     */
-    public function addShow(\Acts\CamdramBundle\Entity\Show $shows)
-    {
-        $this->shows[] = $shows;
-
-        return $this;
-    }
-
-    /**
-     * Remove shows
-     *
-     * @param \Acts\CamdramBundle\Entity\Show $shows
-     */
-    public function removeShow(\Acts\CamdramBundle\Entity\Show $shows)
-    {
-        $this->shows->removeElement($shows);
-    }
-
-    /**
-     * Get shows
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getShows()
-    {
-        return $this->shows;
     }
 
     public function getIndexDate()
